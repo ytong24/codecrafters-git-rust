557d8a8c0d5ec0b50c8655842cfdc82498612d65
Yan Tong <132377526+ytong24@users.noreply.github.com>
2024-03-19 17:54:04 -0700

    Second Exercise

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use anyhow::Context;
 use clap::{Parser, Subcommand};
+use flate2::read::ZlibDecoder;
 use std::fs;
+use std::io::{prelude::*, BufReader};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -13,11 +16,21 @@ struct Args {
 enum Command {
     /// Doc comment
     Init,
+    CatFile {
+        #[clap(short = 'p')]
+        pretty_print: bool,
+
+        object_hash: String,
+    },
 }
 
-fn main() {
+enum Kind {
+    Blob,
+}
+
+fn main() -> anyhow::Result<()> {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    eprintln!("Logs from your program will appear here!");
 
     let args = Args::parse();
 
@@ -29,5 +42,76 @@ fn main() {
             fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
             println!("Initialized git directory")
         }
+        Command::CatFile {
+            pretty_print,
+            object_hash,
+        } => {
+            anyhow::ensure!(
+                pretty_print,
+                "mode must be give without -p, and we don't support mode"
+            );
+            // read the file
+            let f = fs::File::open(format!(
+                ".git/objects/{}/{}",
+                &object_hash[..2],
+                &object_hash[2..]
+            ))
+            .context("Open in .git/objects")?;
+
+            // decompress and parse the header
+            let z = ZlibDecoder::new(f);
+            let mut z = BufReader::new(z);
+            let mut buf = Vec::new();
+            z.read_until(0, &mut buf)
+                .context("read header from .git/objects")?;
+
+            let header = std::ffi::CStr::from_bytes_until_nul(&buf)
+                .expect("know there is exactly one null, and it's at the end");
+            let header = header
+                .to_str()
+                .context(".git/objects file header isn't valid UTF-8")?;
+
+            let Some((kind, size)) = header.split_once(' ') else {
+                anyhow::bail!(
+                    ".git/objects file header did not start with a known type: '{header}'"
+                );
+            };
+
+            let kind = match kind {
+                "blob" => Kind::Blob,
+                _ => anyhow::bail!("we do not yet know how to print a '{kind}'"),
+            };
+
+            let size = size
+                .parse::<usize>()
+                .context(".git/objects file header has invalid size: {size}")?;
+
+            // read the content
+            buf.clear();
+            buf.reserve_exact(size);
+            buf.resize(size, 0);
+
+            z.read_exact(&mut buf)
+                .context("read true contents of .git/objects file")?;
+            // check if the content size matches the size in header
+            let n = z
+                .read(&mut [0])
+                .context("validate EOF in .git/objects file")?;
+            anyhow::ensure!(n == 0, ".git/object file had {n} trailing bytes");
+
+            // since the blob might be an image, println! cannot be used here because it only takes a string.
+            // instead, use stdout directly
+            let stdout = std::io::stdout();
+            let mut stdout = stdout.lock();
+
+            match kind {
+                Kind::Blob => {
+                    stdout
+                        .write_all(&buf)
+                        .context("write object contents to stdout")?;
+                }
+            }
+        }
     }
+    Ok(())
 }
