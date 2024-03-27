aa094dc95e87c7cab05bd5a4f122cb82e3f6cbcf
Yan Tong <132377526+ytong24@users.noreply.github.com>
2024-03-26 17:04:48 -0700

    Finish exercise 3

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use anyhow::Context;
+use anyhow::{Context, Ok};
 use clap::{Parser, Subcommand};
 use flate2::read::ZlibDecoder;
+use flate2::write::ZlibEncoder;
+use flate2::Compression;
+use sha1::{Digest, Sha1};
 use std::fs;
 use std::io::{prelude::*, BufReader};
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -21,6 +25,12 @@ enum Command {
         pretty_print: bool,
 
         object_hash: String,
+    },
+    HashObject {
+        #[clap(short = 'w')]
+        write_object: bool,
+
+        file_path: PathBuf,
     },
 }
 
@@ -110,6 +120,54 @@ fn main() -> anyhow::Result<()> {
                         .write_all(&buf)
                         .context("write object contents to stdout")?;
                 }
+            }
+        }
+        Command::HashObject {
+            write_object,
+            file_path,
+        } => {
+            // get the file stat through metadata before read the content
+            let stat = fs::metadata(&file_path)
+                .with_context(|| format!("stat {}", file_path.display()))?;
+
+            // add header into the hasher
+            let mut hasher = Sha1::new();
+
+            let header = format!("blob {}\0", stat.len());
+            hasher.update(&header);
+
+            // read the file content
+            let file = fs::File::open(&file_path).context("open the file to calculate SHA 1")?;
+            let mut buf_reader = BufReader::new(file);
+            let mut buf = Vec::new();
+            buf_reader
+                .read_to_end(&mut buf)
+                .context("read the content of the file")?;
+
+            // add the content into the hasher
+            hasher.update(&buf);
+
+            let hash_value = hasher.finalize();
+            let hash_value = hex::encode(hash_value); // use hex::encode to change Vec<u8> to string
+            println!("{hash_value}");
+
+            // if write_object is true, compress the blob object and write to the file
+            if write_object {
+                let mut e = ZlibEncoder::new(Vec::new(), Compression::default());
+                e.write_all(&header.as_bytes())
+                    .context("add header into Zlib encoder")?;
+                e.write_all(&buf)
+                    .context("add file content into Zlib encoder")?;
+                let compressed = e.finish().context("compress header and file content")?;
+
+                // create the parent directory to avoid write failure
+                fs::create_dir_all(format!(".git/objects/{}", &hash_value[..2]))
+                    .context("create parent dir for the blob")?;
+                fs::write(
+                    format!(".git/objects/{}/{}", &hash_value[..2], &hash_value[2..]),
+                    &compressed,
+                )
+                .context("write compressed content into blob")?;
             }
         }
     }
