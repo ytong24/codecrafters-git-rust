ef2706ca4bcb3a3455ab6fc95c9114de799e658d
Yan Tong <132377526+ytong24@users.noreply.github.com>
2024-03-28 22:58:22 -0700

    Implement commit-tree

---
+++ src/commands/commit_tree.rs
@@ -0,0 +1,43 @@
+use std::fmt::Write;
+use std::io::Cursor;
+
+use anyhow::Context;
+
+use crate::objects::{Kind, Object};
+
+pub(crate) fn invoke(
+    parent_hash: Option<String>,
+    message: &str,
+    tree_hash: &str,
+) -> anyhow::Result<()> {
+    // the ?s will never be triggered since we are writing into a String
+    let mut commit = String::new();
+
+    writeln!(commit, "tree {}", tree_hash)?;
+    if let Some(parent_hash) = parent_hash {
+        writeln!(commit, "parent {}", parent_hash)?;
+    }
+    writeln!(
+        commit,
+        "author Yan Tong <132377526+ytong24@users.noreply.github.com> 1711657394 -0700"
+    )?;
+    writeln!(
+        commit,
+        "committer Yan Tong <132377526+ytong24@users.noreply.github.com> 1711657394 -0700"
+    )?;
+
+    writeln!(commit, "")?;
+    write!(commit, "{message}")?;
+
+    let hash_value = Object {
+        kind: Kind::Commit,
+        expected_size: commit.len() as u64,
+        reader: Cursor::new(commit),
+    }
+    .write_to_object()
+    .context("write commit object")?;
+
+    println!("{}", hex::encode(hash_value));
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -36,6 +36,15 @@ enum Command {
         tree_hash: String,
     },
     WriteTree,
+    CommitTree {
+        #[clap(short = 'p')]
+        parent_hash: Option<String>,
+
+        #[clap(short = 'm')]
+        message: String,
+
+        tree_hash: String,
+    },
 }
 
 fn main() -> anyhow::Result<()> {
@@ -52,19 +61,29 @@ fn main() -> anyhow::Result<()> {
             fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
             println!("Initialized git directory")
         }
+
         Command::CatFile {
             pretty_print,
             object_hash,
         } => commands::cat_file::invoke(pretty_print, &object_hash)?,
+
         Command::HashObject {
             write_object,
             file_path,
         } => commands::hash_object::invoke(write_object, &file_path)?,
+
         Command::LsTree {
             name_only,
             tree_hash,
         } => commands::ls_tree::invoke(name_only, &tree_hash)?,
+
         Command::WriteTree => commands::write_tree::invoke()?,
+
+        Command::CommitTree {
+            parent_hash,
+            message,
+            tree_hash,
+        } => commands::commit_tree::invoke(parent_hash, &message, &tree_hash)?,
     }
     Ok(())
 }
